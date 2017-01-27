f5a9d70a2f6d7ed0706a48dae0308bce0091e5a5
ISHII Takeru <ishii-t@mail.impact-e.jp>
2017-01-27 12:32:38 +0900

    テスト追加　HOMEROOT画面

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+
+  #------------------------------------------------------------------
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   #static_pages/home がGETメソッドで正常に動作するかのテスト
   test "should get home" do
     get static_pages_home_url
