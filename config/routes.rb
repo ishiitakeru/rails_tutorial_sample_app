19fd25c597d801adbf392b5df595fa413ce88bad
ISHII Takeru <ishii-t@mail.impact-e.jp>
2017-01-30 17:35:47 +0900

    パーシャル利用、SCSS利用、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   get 'application/hello'
   get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+
+  # get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+
+  # get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+
+  #get 'static_pages/contact'
+  # 以下の書き方をすると「_path」「_url」のlink_to指定が有効になる。
+  get '/contact', to: 'static_pages#contact'
+
+  get '/login', to: 'static_pages#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,30 +16,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   #static_pages/home がGETメソッドで正常に動作するかのテスト
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
   #static_pages/help がGETメソッドで正常に動作するかのテスト
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   #static_pages/about がGETメソッドで正常に動作するかのテスト
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   #static_pages/contact がGETメソッドで正常に動作するかのテスト
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+
 
 end
