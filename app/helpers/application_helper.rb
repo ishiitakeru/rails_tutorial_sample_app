666214f8537b87fa04b9f0f8e310f4299a92e36e
ISHII Takeru <ishii-t@mail.impact-e.jp>
2017-01-30 12:56:44 +0900

    Rails風味のRubyの章の分

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  #------------------------------------------------------------------
+  # ページごとに完全なタイトルを返す
+  # Viewでの使い方 : <%= full_title("HOME") %>
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+
+    #  falseまたはnil
+    if !page_title    || page_title.empty?
+      base_title
+    else
+      base_title = page_title + " | " + base_title
+    end
+  end
 end
