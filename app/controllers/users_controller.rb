c98eedb1397e85c40d409c3b9ffa77e9263c603a
ISHII Takeru <ishii-t@mail.impact-e.jp>
2017-01-31 18:46:52 +0900

    7.1.3 debuggerメソッド あたりまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  # 新規登録
   def new
   end
+
+  # 詳細表示
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,12 @@ Rails.application.routes.draw do
   # get 'users/new'
   get '/signup', to: 'users#new'
 
+  # ユーザーテーブルに基づくリソース
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #root "application#hello"
   root "static_pages#home"
+
 end
